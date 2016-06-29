ecd8b925a2de9b986e8924c088c4811eff6a395e
Armen Rostamian <arostamian@ulive.com>
2016-06-29 12:14:09 -0400

    more fun with smart routes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,26 @@
 class RecipesController < ApplicationController
+  def index
+    @recipes = Recipe.all
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    recipes_params = params.require(:recipe).permit(
+      :name, :description, :preparation_time, :servings_yielded)
+    @recipe = Recipe.new(recipes_params)
+
+    if @recipe.save
+      redirect_to recipe_path(@recipe)
+    else
+      render :new
+    end
+  end
+
 end
