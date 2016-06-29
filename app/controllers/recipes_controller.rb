f88c6ea750cf46ecaa1ffe07ce68066e70a0b17d
Armen Rostamian <arostamian@ulive.com>
2016-06-29 13:38:11 -0400

    resourceful routing - complete

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -12,7 +12,7 @@ class RecipesController < ApplicationController
   end
 
   def create
-    @recipe = Recipe.new(recipes_params)
+    @recipe = Recipe.new(recipe_params)
 
     if @recipe.save
       redirect_to recipe_path(@recipe)
