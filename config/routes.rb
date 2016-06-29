db6f1f2ec6ae31d48681bd1594444c1ebba724fc
Armen Rostamian <arostamian@ulive.com>
2016-06-29 14:01:14 -0400

    fucking likes, brug

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :recipes do
+    patch :like, on: :member
     resources :ingredients, except: [:index, :show]
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160629165651) do
+ActiveRecord::Schema.define(version: 20160629174202) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,8 +32,9 @@ ActiveRecord::Schema.define(version: 20160629165651) do
     t.text     "description"
     t.integer  "preparation_time"
     t.integer  "servings_yielded"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+    t.integer  "likes",            default: 0
   end
 
   add_foreign_key "ingredients", "recipes"
